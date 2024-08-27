75ea570e1393453c76be29918848052e63bdd33d
Ansar <mohammadkhaleelansarsk.21@gmail.com>
2024-08-27 12:04:06 +0530

    updates day-5

---
+++ day-5/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "Ansar" { 
+    ami = "ami-02b49a24cfb95941c"
+    instance_type = "t2.micro"
+    key_name = "Sikindar" 
+
+    
+   lifecycle {
+    create_before_destroy = true    #this attribute will create the new object first and then destroy the old one
+  } 
+
+    
+ }
+
+# lifecycle {
+#   prevent_destroy = true   #Terraform will error when it attempts to destroy a resource when this is set to true:
+# }
+
+
+#   lifecycle {
+#     ignore_changes = [tags,] #This means that Terraform will never update the object but will be able to create or destroy it.
+#   }
