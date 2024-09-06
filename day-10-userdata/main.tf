8f672338087d0e6f3afa3ff5d60e9356d4f0a870
Ansar <mohammadkhaleelansarsk.21@gmail.com>
2024-09-06 08:24:14 +0530

    updates

--- day-10-userdata/main.tf
+++ day-10-userdata/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
     ami = "ami-02b49a24cfb95941c"
     instance_type = "t2.micro"
-    availability_zone = "ap-south-1"
+    availability_zone = "ap-south-1a"
     key_name = "Sikindar"
     user_data = file("test.sh")
     
